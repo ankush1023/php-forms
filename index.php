b7a6065779261ef2951b2d00ebe7a9af11f2264c
ankush sharma <ankush.cloveode@gmail.com>
2023-12-27 14:23:42 +0530

    creating get form

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="/css/bootstrap.css">
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+
+    </head>
+    <body>
+
+    </body>
+
+</html>
